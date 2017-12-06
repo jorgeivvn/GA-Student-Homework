8d698b00dd59a5bf1d95253c0876dbbde41f2237
Jorge Rodriguez <j_rod15@msn.com>
2017-12-05 23:16:41 -0600

    Commit 9 - created loop.js file

---
+++ Week_One/resume_page/loops.js
@@ -0,0 +1,31 @@
+for(let i=0; i <= 99; i++) {
+  console.log(i);
+}
+
+for(let i=99; i >= 0; i--) {
+  console.log(i);
+}
+
+for(let i=0; i <= 98; i++) {
+  if(i % 2 == 0);
+  console.log(i);
+}
+
+for(let i=98; i >= 0; i--) {
+  if(i % 2 == 0);
+  console.log(i);
+}
+
+for(let i=49; i <= 72; i++) {
+  console.log(i);
+}
+
+for(let i=81; i >= 26; i--) {
+  console.log(i);
+}
+
+//Need help with this last one, multiples of 3's
+for(let i=3; i <= 90; i++){
+  if(i % 3 == 0);
+  console.log(i);
+}
