7acca8872948bdb98b55917f699486e2610f600c
Jorge Rodriguez <j_rod15@msn.com>
2017-12-07 23:41:58 -0600

    Commit 5 - Sum Array

--- Week_One/Thursday/solutions.js
+++ Week_One/Thursday/solutions.js
@@ -3,7 +3,7 @@
 // Within a function, what is the difference between return and console.log?
 //The return value can be used to pass on data, whereas you can't do the same with the console.log
 
-//
+//Couldn't figure out how to make it work with capital letter
 const checkPalindrome = (word) => {
   if (word == word.toLowerCase("").split("").reverse().join("")) {
     return true
@@ -14,7 +14,7 @@ const checkPalindrome = (word) => {
 
 console.log(checkPalindrome("racecar"));
 
-//Commit 2 - Palindrome
+//Digit Sum
 const sumDigits = (num) => {
   let sum = 0;
   let add = num.toString().split('');
@@ -26,7 +26,7 @@ const sumDigits = (num) => {
 
 console.log(sumDigits(42));
 
-//Commit 3 - Digit Sum
+//Pythagorean Theorem
 
 const calculateSide = (sideA, sideB) => {
   let pyth = Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
@@ -35,4 +35,15 @@ const calculateSide = (sideA, sideB) => {
 
 console.log(calculateSide(8,6));
 
-//Commit 4 - Pythagoras
+//Commit 4 - Pythagoras //
+
+//can't figure out
+const sumArray = (array) => {
+  let sum = 0;
+  let array=
+  for (var i = 0; i < array.length; i++) {
+    return sum
+  }
+}
+
+console.log(sumArray([1, 2, 3, 4, 5, 6]));
