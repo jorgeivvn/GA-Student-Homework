4314f97c1235c2b7b1d4f8b44fe794cbfc0efc08
Jorge Rodriguez <j_rod15@msn.com>
2017-12-11 19:03:18 -0600

    2nd commit - created some data structures and accessed some data

--- Week_Two/Monday/answers.js
+++ Week_Two/Monday/answers.js
@@ -4,3 +4,18 @@
 //4. Array
 //5. Object
 //6. Object
+
+//TAKE IT EASY
+const rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+console.log(rainbow[4]);
+
+
+const jorge = {
+  favoriteFood: "pizza",
+  hobby: "basketball",
+  currentCity: "Dallas",
+  favoriteDatatype: "Objects",
+}
+
+console.log(jorge["hobby"]);
