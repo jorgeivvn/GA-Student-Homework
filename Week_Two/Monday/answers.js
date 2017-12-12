2d375db7ef9fadcd98a88b9a0c0dbfceba8e8eb4
Jorge Rodriguez <j_rod15@msn.com>
2017-12-11 20:04:36 -0600

    4th Commit - accessed limbo

--- Week_Two/Monday/answers.js
+++ Week_Two/Monday/answers.js
@@ -57,3 +57,26 @@ console.log(crazyObject.larry.characters[2].favourtieHobby);
 console.log(crazyObject.larry.nicknames[1]);
 
 console.log(crazyObject.larry.characters[1]);
+
+//Object-ception
+const inception = {
+   reality: {
+       dreamLayer1: {
+           dreamLayer2: {
+               dreamLayer3: {
+                   dreamLayer4: {
+                       dreamLayer5: {
+                           dreamLayer6: {
+                               limbo: "Joseph Gordon Levitt"
+                          }
+                       }
+                   }
+               }
+           }
+       }
+   }
+}
+
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
+
+console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo);
