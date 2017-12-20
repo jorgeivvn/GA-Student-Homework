c05b77fc2fddc8c6c3594299c3a0ccc8b58c0d7c
Jorge Rodriguez <j_rod15@msn.com>
2017-12-19 23:53:55 -0600

    2nd commit - JS updated

--- week3/tuesday/app.js
+++ week3/tuesday/app.js
@@ -1,8 +1,16 @@
+var endpoint = 'https://api.fixer.io/latest?base=USD'
+
+var amountContainer = document.getElementById('output_amount')
+
+var currencyContainer = document.getElementById('output_currency')
 
 
 function convertCurrency(event) {
   if ('type' in event.target && event.target.type === 'button') {
     console.log('handle currency conversion here');
+    $.get(endpoint, (response) => {
+      processResponse(response);
+    })
   }
 }
 
